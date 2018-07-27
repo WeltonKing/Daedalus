f4ac3e62fde3c702b7f680ca0102a68091ca0cfb
MasonAMcCallum <masonamccallum@gmail.com>
2018-07-26 20:07:32 -0500

    large update

---
+++ Bitmap.cpp
@@ -0,0 +1,15 @@
+#include "Bitmap.h"
+
+namespace mazeSpace{
+	Bitmap::Bitmap(int width, int height): m_width(width),m_height(height) {
+		
+	}
+	
+	Bitmap::~Bitmap() {
+
+	}
+
+	bool Bitmap::write(string filename){
+		return true;
+	}
+}
